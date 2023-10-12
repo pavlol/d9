2bb271c4422bc99af54090647b65f4c6f2d9b322
Pavl <pavl@MacBook-Air-von-Pavl.local>
2023-10-12 16:52:52 +0200

    basic working TF: EC2, S3, VPC create

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-#Resource to create s3 bucket
+#Resource to create s3 bucket - works
 # resource "aws_s3_bucket" "pavlols3testb1"{
 #   bucket = "pavlols3testb1"
 
@@ -24,5 +24,71 @@ provider "aws" {
 #   }
 # }
 
+# Upload an object - LOCK Error
+# resource "aws_s3_bucket_object" "object" {
 
+#   bucket = aws_s3_bucket.pavlols3testb1.id
+
+#   key    = "profile"
+#   acl = "public-read"
+#   #acl    = "private"  # or can be "public-read"
+
+#   source = "${path.module}/terravars.png"
+#   # source = "myfiles/yourfile.txt"
+
+#   etag = filemd5("${path.module}/terravars.png")
+
+# }
+
+# create ec2
+
+resource "aws_instance" "myEC2" {
+  ami = "ami-09100e341bda441c0"
+  
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ec2pavlodeham91210"
+  }
+}
+
+
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "LYakhov VPC"
+ }
+}
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
 
