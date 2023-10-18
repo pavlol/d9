bcf44a4203eda10ee806f15d2add1e96ed1048ea
Pavl <pavl@MacBook-Air-von-Pavl.local>
2023-10-18 17:48:02 +0200

    manual config for lab, not finished

--- main.tf
+++ main.tf
@@ -14,13 +14,111 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-#Resource to create s3 bucket - works
-resource "aws_s3_bucket" "pavlols3testb1"{
-  bucket = "pavlols3testb1"
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ instance_tenancy = "default"
+
+ tags = {
+   Name = "Lyakhov VPC"
+ }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
   tags = {
-    Name = "S3Bucket"
+    Name = "main gateway"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Public subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/23"
+
+  tags = {
+    Name = "Private subnet"
+  }
+}
+
+resource "aws_nat_gateway" "natgw" {
+  subnet_id = aws_subnet.public_subnet.id
+  tags = {
+    Name = "NAT gw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_default_route_table" "public_rt" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name = "public route table"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name = "private route table"
+  }
+}
+
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "public_subnet_assoc"{
+    count = 1
+    route_table_id = aws_default_route_table.public_rt.id
+    subnet_id = aws_subnet.public_subnet.id
+    depends_on = [aws_route_table.public_route,aws_subnet.public_subnet]    
+}
+
+resource "aws_route_table_association" "private_subnet_assoc"{
+    count = 1
+    route_table_id = aws_route_table.private_rt.id
+    subnet_id = aws_subnet.private_subnet.id
+    depends_on = [aws_route_table.private_route,aws_subnet.private_subnet]    
+}
+
+
+#Resource to create s3 bucket - works
+# resource "aws_s3_bucket" "pavlols3testb1"{
+#   bucket = "pavlols3testb1"
+#   tags = {
+#     Name = "S3Bucket"
+#   }
+# }
 
 # Upload an object - LOCK Error
 # resource "aws_s3_bucket_object" "object" {
@@ -35,84 +133,70 @@ resource "aws_s3_bucket" "pavlols3testb1"{
 #   etag = filemd5("${path.module}/terravars.png")
 # }
 
+# Security Group Creation for provisionerVPC
+
+resource "aws_security_group" "dev_terraform_sg_allow_ssh_http"{
+    name="dev-sg"
+    vpc_id = aws_vpc.main.id
+}
+
+# Ingress Security Port 22 (Inbound)
+resource "aws_security_group_rule" "ssh_ingress_access"{
+    from_port = 22
+    protocol = "tcp"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 22
+    type = "ingress"
+    cidr_blocks = "10.0.0.0/16"
+}
+
+# Ingress Security Port 80 (Inbound)
+resource "aws_security_group_rule" "http_ingress_access"{
+    from_port = 80
+    protocol = "tcp"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 80
+    type = "ingress"
+    cidr_blocks = "10.0.0.0/16"
+}
+
+# All egress/outbound Access
+
+resource "aws_security_group_rule" "all_egress_access"{
+    from_port = 0
+    protocol = "-1"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 0
+    type = "egress"
+    cidr_blocks = "10.0.0.0/16"
+}
+
+
 # create ec2
 resource "aws_instance" "myEC-2" {
   ami = "ami-09100e341bda441c0"
   instance_type = "t2.micro"
+  associate_public_ip_address = false
+  vpc_security_group_ids = [aws_security_group.dev_terraform_sg_allow_ssh_http.id]
+  subnet_id = aws_subnet.public_subnet.id
   tags = {
-    Name = "ec2pavlodeham91210"
+    Name = "ec2_pc"
   }
 }
 
-
-resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
- instance_tenancy = "default"
-
- tags = {
-   Name = "LYakhov VPC"
- }
-}
-
-variable "public_subnet_cidrs" {
- type        = list(string)
- description = "Public Subnet CIDR values"
- default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
- 
-variable "private_subnet_cidrs" {
- type        = list(string)
- description = "Private Subnet CIDR values"
- default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-}
-
-resource "aws_subnet" "public_subnets" {
- count      = length(var.public_subnet_cidrs)
- vpc_id     = aws_vpc.main.id
- cidr_block = element(var.public_subnet_cidrs, count.index)
- 
- tags = {
-   Name = "Public Subnet ${count.index + 1}"
- }
-}
- 
-resource "aws_subnet" "private_subnets" {
- count      = length(var.private_subnet_cidrs)
- vpc_id     = aws_vpc.main.id
- cidr_block = element(var.private_subnet_cidrs, count.index)
- 
- tags = {
-   Name = "Private Subnet ${count.index + 1}"
- }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
+resource "aws_instance" "myBastion" {
+  ami = "ami-09100e341bda441c0"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.private_subnet.id
+  vpc_security_group_ids = [aws_security_group.dev_terraform_sg_allow_ssh_http.id]
   tags = {
-    Name = "main gateway"
+    Name = "bastion"
   }
 }
 
-resource "aws_default_route_table" "rt" {
-  default_route_table_id = aws_vpc.main.default_route_table_id
 
-  #"10.0.1.0/24", "10.0.1.0/24", "10.0.3.0/24"
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  route {
-    cidr_block = "10.0.2.0/24"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  route {
-      cidr_block = "10.0.3.0/24"
-      gateway_id = aws_internet_gateway.gw.id
-    }
-  tags = {
-    Name = "defaul route table"
-  }
+resource "aws_security_group" "dev_terraform_sg_allow_ssh_http"{
+    name="dev-sg"
+    vpc_id = aws_vpc.main.id
 }
+
